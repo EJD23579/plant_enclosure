c49756abbdaf4bb487a987149cb00cf9bc0862bf
Elliot-Dorey <dorey.elliot@gmail.com>
2022-11-13 22:43:20 +0000

    Basic skeleton of front end

--- src/temperature.js
+++ src/temperature.js
@@ -1,9 +1,28 @@
-import React from "react";
+import { Grid, Slider } from "@mui/material";
+import { alignProperty } from "@mui/material/styles/cssUtils";
+import React, { useState } from "react";
+import ReactSlider from "react-slider";
 
-export default function Temperature(){
-    return (
-        <div>
-            add slider here
-        </div>
+function Temperature(){
+    const [range, setRange] = useState('50%');
+    const [sliding, setSliding] = useState('Inactive');
+
+    return (<Grid class="slider">
+        <Slider style={{
+        textAlign:"center",
+        width:500,
+       }}
+        aria-label="Humidity"
+        defaultValue={50}
+        valueLabelDisplay="auto"
+        min={0}
+        max={100}>
+        
+
+            </Slider>
+            </Grid>
     )
 }
+
+
+export default Temperature
