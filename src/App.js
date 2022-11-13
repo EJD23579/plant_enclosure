74f31a279249b8a3c607989d70b6402560ff95d4
Elliot-Dorey <dorey.elliot@gmail.com>
2022-11-13 21:26:33 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Humidity from './humidity.js';
+import Temperature  from './temperature.js';
+import Light from './light.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Plant Enclosure </h1>
+      const 
+      <h2> Humidity (%) </h2>
+      <h3> current humidity here </h3>
+      <button> Set target humidity </button>
+      <Humidity></Humidity>
+      <h2> Light Level (lx) </h2>
+      <h3> current light level here </h3>
+      <button> Set target light level </button>
+      <Light></Light>
+      <button> daylight tracking on/off </button>
+      <h2> Temperature (degrees celsius) </h2>
+      <button> Set target Temperature </button>
+      <Temperature></Temperature>
+
+
+
+
+
     </div>
   );
 }
---
+++ src/light.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Light(){
+    return (
+        <div>
+            add slider here
+        </div>
+    )
+}
